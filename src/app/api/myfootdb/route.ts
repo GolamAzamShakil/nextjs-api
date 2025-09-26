835949c2d468bc505bb2f3469d666d25fe2d208b
GolamAzamShakil <golamazam1010@gmail.com>
2025-09-26 23:11:35 +0600

    crud

--- src/app/api/myfootdb/route.ts
+++ src/app/api/myfootdb/route.ts
@@ -21,6 +21,135 @@ export const GET = async () => {
   }
 };
 
+/* export const POST = async (request: Request) => {
+  try {
+    const body = await request.json();
+    const { userId, userName, userEmail, userPassword, isMfaEnabled } = body;
+
+    if (
+      !userId ||
+      !userName ||
+      !userEmail ||
+      !userPassword ||
+      typeof isMfaEnabled !== "boolean"
+    ) {
+      return NextResponse.json(
+        { message: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+    await connectDB();
+
+    const hashedPassword = await argon2.hash(userPassword);
+
+    const newUser = new User({
+      userId,
+      userName,
+      userEmail,
+      userPassword: hashedPassword,
+      isMfaEnabled,
+    });
+    await newUser.save();
+
+    const { userPassword: _, ...userWithoutPassword } = newUser.toObject();
+
+    return NextResponse.json(userWithoutPassword, { status: 201 });
+  } catch (error: any) {
+    return new NextResponse(
+      "Error occurred during creating user- " + error.message,
+      { status: 500 }
+    );
+  }
+}; */
+
+export const PATCH = async (request: Request) => {
+  try {
+    const body = await request.json();
+    const { userId, newUserName } = body;
+
+    await connectDB();
+
+    if (!userId || !newUserName) {
+      return new NextResponse(
+        JSON.stringify({ message: "ID or new user name not found" }),
+        { status: 400 }
+      );
+    }
+
+    if (!Types.ObjectId.isValid(userId)) {
+      return new NextResponse(JSON.stringify({ message: "Invalid User ID" }), {
+        status: 400,
+      });
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: new ObjectId(userId) },
+      { userName: newUserName },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return new NextResponse(
+        JSON.stringify({ message: "User is not found in database" }),
+        { status: 400 }
+      );
+    }
+
+    return new NextResponse(
+      JSON.stringify({ message: "User is updated", user: updatedUser }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return new NextResponse(
+      "Error occurred during updating user- " + error.message,
+      { status: 500 }
+    );
+  }
+};
+
+export const DELETE = async (request: Request) => {
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId) {
+      return new NextResponse(
+        JSON.stringify({ message: "User ID not found" }),
+        { status: 400 }
+      );
+    }
+
+    if (!Types.ObjectId.isValid(userId)) {
+      return new NextResponse(JSON.stringify({ message: "Invalid User ID" }), {
+        status: 400,
+      });
+    }
+
+    await connectDB();
+
+    const deleteUser = await User.findByIdAndDelete(new Types.ObjectId(userId));
+
+    if (!deleteUser) {
+      return new NextResponse(
+        JSON.stringify({ message: "User is not found in database" }),
+        { status: 400 }
+      );
+    }
+
+    return new NextResponse(
+      JSON.stringify({
+        message: "User is removed from database",
+        user: deleteUser,
+      }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return new NextResponse(
+      "Error occurred during removing user- " + error.message,
+      { status: 500 }
+    );
+  }
+};
 
 /* if () {
     return new NextResponse(JSON.stringify(), { status: } );
