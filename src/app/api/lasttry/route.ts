d916caf3ce6af92b5436f01e841f06e457a2bcf5
GolamAzamShakil <golamazam1010@gmail.com>
2025-09-26 22:38:23 +0600

    crud

---
+++ src/app/api/lasttry/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import connectDB from "../../../../lib/db";
+import Product from "../../../../lib/models/products";
+import User from "../../../../lib/models/users";
+
+
+export const GET = async () => {
+    try {
+        await connectDB();
+
+        const products = await Product.find({}).populate("productDetails");
+        const users = await User.find({}, "-userPassword");
+
+        return new NextResponse(JSON.stringify(products), { status: 200 });
+    } catch (error: any) {
+        return new NextResponse("Error occurred during fetching users- " + error.message, { status: 500 })
+    }
+}
