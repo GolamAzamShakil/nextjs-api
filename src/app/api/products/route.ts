f6a583e75dfff302f723c2670d8c28407bf6a1b9
GolamAzamShakil <golamazam1010@gmail.com>
2025-09-26 19:05:34 +0600

    crud

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -11,7 +11,7 @@ export const GET = async () => {
         const products = await Product.find({}).populate("productDetails");
         const users = await User.find({}, '-userPassword');
 
-        return new NextResponse(JSON.stringify(products, users), { status: 200 });
+        return new NextResponse(JSON.stringify( users), { status: 200 });
     } catch (error: any) {
         return new NextResponse("Error occurred during fetching users- " + error.message, { status: 500 })
     }
