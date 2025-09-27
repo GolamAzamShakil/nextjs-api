2a8d63729245bcd7ede860e5fcd19ed381a51cd6
GolamAzamShakil <golamazam1010@gmail.com>
2025-09-27 10:52:00 +0600

    argon2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+//const nextConfig = {};
+const OPTIMIZE_OUTPUT = true;
+
+const nextConfig = {
+  ...(OPTIMIZE_OUTPUT && {
+    output: "standalone",
+  }),
+  experimental: {
+    outputFileTracingIncludes: {
+      "/": ["./node_modules/argon2/prebuilds//.node"],
+    },
+  },
+};
 
 export default nextConfig;
