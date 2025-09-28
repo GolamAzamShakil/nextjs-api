b1326139bde703dd62d003ff9d662632446a24eb
GolamAzamShakil <golamazam1010@gmail.com>
2025-09-28 12:10:46 +0600

    pr update

--- lib/linkConverter.ts
+++ lib/linkConverter.ts
@@ -2,8 +2,9 @@ export function convertGoogleDriveUrl(url: string): string | null {
   const match = url.match(/file\/d\/([^\/]+)\//);
   if (match && match[1]) {
     const fileId = match[1];
-    const newURL = `https://drive.google.com/uc?export=download&id=${fileId}`
-    return newURL;
+    //const newDownloadURL = `https://drive.google.com/uc?export=download&id=${fileId}`
+    const newDirectURL = `https://drive.google.com/thumbnail?id=${fileId}`
+    return newDirectURL;
   }
   return null;
 }
